75204b857b2dd25fcf994bd056a3aa3cfad16478
steve <steven.b.perry@gmail.com>
2024-04-02 10:44:35 -0500

    Pairing mode changes

--- MCU/STM32CubeIDE_Project/cdp_project/Core/Src/HC05.c
+++ MCU/STM32CubeIDE_Project/cdp_project/Core/Src/HC05.c
@@ -19,13 +19,15 @@
 void HC05_pair(int* pairing_flag, UART_HandleTypeDef huart1){
 	// sets the HC05 into AT mode
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
-	char pData[] = "STM32 in AT mode!";
-	int Size = sizeof(pData);
-	HAL_UART_Transmit(&huart1, (uint8_t*) &pData, Size, 100);
+	char data1[] = "STM32 in pairing mode!";
+	int size = sizeof(data1);
+	HAL_UART_Transmit(&huart1, (uint8_t*) &data1, size, 100);
 	while (*pairing_flag == 0) {
 		// wait
 	}
-	// turns off AT mode
+	char data2[] = "STM32 leaving pairing mode!";
+	size = sizeof(data2);
+	HAL_UART_Transmit(&huart1, (uint8_t*) &data2, size, 100);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
 
 }
