3df6a215c2f3ac420e30973d4a3fd85e393a60cc
Steven <sbp0023@auburn.edu>
2024-04-14 10:21:04 -0500

    Increased RX buffer for magnetic data reception

--- MCU_H7/cdp_H7_implementation/Core/Inc/main.h
+++ MCU_H7/cdp_H7_implementation/Core/Inc/main.h
@@ -36,7 +36,7 @@ extern "C" {
 #include "string.h"
 #include "math.h"
 
-#define RX_BUFFER_SIZE 19 // RX buffer can hold up to 13 bytes
+#define RX_BUFFER_SIZE 35 // RX buffer can hold up to 13 bytes
 
 #define ALT_MOTOR 1
 #define AZ_MOTOR 2
